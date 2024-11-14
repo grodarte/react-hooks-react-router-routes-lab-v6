0b1ef437db7f7f60647b437207feb0ba484d4fbe
Gabrielle <ms.grodarte@gmail.com>
2024-11-13 20:19:09 -0800

    complete lab

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,13 +1,41 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([])
+
+  useEffect(()=>{
+    fetch(`http://localhost:4000/directors`)
+    .then(r=>r.json())
+    .then(directorData=>setDirectors(directorData))
+    .catch(error=>console.log(error))
+  }, [])
+
+  if(!directors){
+    return <span>Loading...</span>
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directors.map(director=>{
+          return (
+            <article key={director.id}>
+              <h2>{director.name}</h2>
+              <ul>
+                {director.movies.map(movie=>{
+                  return (
+                    <li key={movie}>{movie}</li>
+                  )
+                })}
+              </ul>
+            </article>
+          )
+        })}
       </main>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard"
 
 function Home() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(()=>{
+    fetch(`http://localhost:4000/movies`)
+    .then(r=>r.json())
+    .then(movieData=>setMovies(movieData))
+    .catch(error=>console.log(error))
+  }, [])
+
+  const movieElements = movies.map(movie=><MovieCard key={movie.id} movie={movie} />)
+
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        {movieElements}
       </main>
     </>
   );
